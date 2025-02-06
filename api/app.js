e8535f357050366d552c1c15e0831bcefc102902
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2025-02-06 12:51:24 +0300

    auth login

--- api/app.js
+++ api/app.js
@@ -2,21 +2,19 @@ import express from "express";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 
-
 const app = express().use(express.json());
-      
+
 // app.use(express.json())
 
 app.use("/api/posts", postRoute);
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
 // app.use("/api/auth/register", (req, res) =>{
 //     res.send("Welcome")
 // })
 
 app.use("/api/posts/id", (req, res) => {
-    res.send("auth works")
-})
-app.listen (8800, () =>{
-    console.log("server is running")
-})
-
+  res.send("auth works");
+});
+app.listen(8800, () => {
+  console.log("server is running");
+});
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,26 +1,49 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
-
-export const register = async(req, res) =>{
-    // destructure data 
-    const {username, email, password} = req.body;
+// REGISTER
+export const register = async (req, res) => {
+  // destructure data
+  const { username, email, password } = req.body;
+  try {
     // hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
     // console.log(hashedPassword);
-
     const newUser = await prisma.user.create({
-        data: {
-            username,
-            email, 
-            password: hashedPassword,
-        }
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
     });
-    console.log(newUser)
-    res.status(201).json({message: "User created successfully"})
+    console.log(newUser);
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    // console.log(error)
+    res.status(500).json({ message: "Failed to create user! Try again." });
+  }
 };
-export const login = (req, res) =>{
-    // db operations
+// LOGIN
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+    // Check if user exists
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
+    // Check if password is correct 
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials!"});
+    }
+    //Generate token to send to user
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({message: "Login failed!"})
+  } // db operations
 };
-export const logout = (req, res) =>{
-    // db operations
+export const logout = (req, res) => {
+  // db operations
 };
